ac5af6d09e860d741e7ef7e9c59e41c8c5e5e1d5
EL AZZAOUY Radouane <rdwanalzawy6@gmail.com>
2025-12-29 15:52:06 +0100

    Clean the repo

--- TP1/obj/Release/net9.0/TP1.AssemblyInfo.cs
+++ TP1/obj/Release/net9.0/TP1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6353f65cc20b981e3ba0112006adc5b070b4f45d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45ff7ab5548b4995813dfd99b55eb5bdd848ec1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
